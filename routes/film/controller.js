76201df9de02e9a3b8388b9390ce52bfcc60f453
ahadee <bagus.ardhika@gmail.com>
2020-06-16 05:19:48 +0700

    add getPopularFilm function to film routes

--- routes/film/controller.js
+++ routes/film/controller.js
@@ -1,4 +1,5 @@
 const {Films} = require('../../models')
+const { Op } = require("sequelize")
 
 module.exports = {
     getAllFilm: async (req,res) => {
@@ -13,5 +14,22 @@ module.exports = {
             
         }
     },
+    getPopulerFilm: async (req,res) => {
+        try {
+            const result = await Films.findAll({
+                where: { 
+                    user_rating: {
+                        [Op.gt]: 6.9 
+                    } 
+                }
+            })
+            res.status(200).json({
+                message: "Get Film data by popularity",
+                data: result
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
     
 }
--- routes/film/index.js
+++ routes/film/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-const {getAllFilm} = require('./controller')
+const {getAllFilm, getPopulerFilm} = require('./controller')
 
 router.get('/',getAllFilm)
+router.get('/populer',getPopulerFilm)
 
 module.exports = router
